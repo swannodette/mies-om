26913caadaa0ffd0e4514ba0d95505c02dbd81dc
David Nolen <david.nolen@gmail.com>
2014-01-20 16:50:15 -0500

    might want to include om

--- src/leiningen/new/mies_om/project.clj
+++ src/leiningen/new/mies_om/project.clj
@@ -5,6 +5,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2138"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
+                 [om "0.2.2"]
                  [com.facebook/react "0.8.0.1"]]
 
   :plugins [[lein-cljsbuild "1.0.1"]]
