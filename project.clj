c7b1a3d461efd0ba13c103af7b39f98f0e038ed6
David Nolen <david.nolen@gmail.com>
2014-01-25 11:40:35 -0500

    bump Om

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mies-om/lein-template "0.1.3"
+(defproject mies-om/lein-template "0.1.4"
   :description "A minimal Om project template"
   :url "http://github.com/swannodette/mies-om"
   :license {:name "Eclipse Public License"
