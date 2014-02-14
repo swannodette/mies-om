d14bee4afdbebcb3d42d672cc6f4b16b1cddd93b
David Nolen <david.nolen@gmail.com>
2014-02-14 00:41:23 -0500

    update template to 4.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mies-om/lein-template "0.1.7"
+(defproject mies-om/lein-template "0.1.8"
   :description "A minimal Om project template"
   :url "http://github.com/swannodette/mies-om"
   :license {:name "Eclipse Public License"
--- src/leiningen/new/mies_om/project.clj
+++ src/leiningen/new/mies_om/project.clj
@@ -3,9 +3,9 @@
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2138"]
+                 [org.clojure/clojurescript "0.0-2156"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [om "0.3.6"]
+                 [om "0.4.0"]
                  [com.facebook/react "0.8.0.1"]]
 
   :plugins [[lein-cljsbuild "1.0.2"]]
