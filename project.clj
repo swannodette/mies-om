cb3020f77657476d56c20f9466c1ea6ff41e4819
David Nolen <david.nolen@gmail.com>
2014-07-12 12:40:53 -0400

    bump deps

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mies-om/lein-template "0.2.4"
+(defproject mies-om/lein-template "0.3.0"
   :description "A minimal Om project template"
   :url "http://github.com/swannodette/mies-om"
   :license {:name "Eclipse Public License"
--- src/leiningen/new/mies_om/project.clj
+++ src/leiningen/new/mies_om/project.clj
@@ -2,12 +2,12 @@
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
 
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2173"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2268"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [om "0.5.0"]]
+                 [om "0.6.5"]]
 
-  :plugins [[lein-cljsbuild "1.0.2"]]
+  :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]]
 
   :source-paths ["src"]
 
