a5ecfe53f36bc1707ec8caf18b4e2ac805c7bc9d
David Nolen <david.nolen@gmail.com>
2014-08-01 19:47:04 -0400

    bump deps

--- src/leiningen/new/mies_om/project.clj
+++ src/leiningen/new/mies_om/project.clj
@@ -3,9 +3,9 @@
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2268"]
+                 [org.clojure/clojurescript "0.0-2280"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [om "0.6.5"]]
+                 [om "0.7.0"]]
 
   :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]]
 
