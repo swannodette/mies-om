2692bb9ff809c64a2a8275360005c65e8d8f112d
Jonas Enlund <jonas.enlund@gmail.com>
2014-12-22 22:29:07 +0200

    Bump cljs version

--- src/leiningen/new/mies_om/project.clj
+++ src/leiningen/new/mies_om/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2498"]
+                 [org.clojure/clojurescript "0.0-2505"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [om "0.8.0-beta5"]]
 
