594ab2a467678b2741b7d9558cb94cc81d538118
David Nolen <david.nolen@gmail.com>
2015-02-01 19:37:28 -0500

    bump to Om 0.8.8

--- src/leiningen/new/mies_om/project.clj
+++ src/leiningen/new/mies_om/project.clj
@@ -5,7 +5,7 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2755"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [org.omcljs/om "0.8.7"]]
+                 [org.omcljs/om "0.8.8"]]
 
   :plugins [[lein-cljsbuild "1.0.4"]]
 
